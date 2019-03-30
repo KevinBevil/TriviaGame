1d8ce04fd222a9e69839e6939610e28a400cd9c8
Kevin Bevil <kbevil@gmail.com>
2019-03-30 15:12:26 -0400

    Added more styling, as well as many questions/answers

---
+++ assets/javascript/app.js
@@ -0,0 +1,47 @@
+var questions = [
+   {
+      question: "Who directed the 1971 version?",
+      answers: {
+         a: "Robert Altman",
+         b: "Mel Stuart",
+         c: "Sidney Lumet"
+      },
+      correctAnswer: "b"
+   },
+   {
+      question: "Which of the following books was not written by Roald Dahl?",
+      answers: {
+         a: "James and the Giant Peach",
+         b: "Matilda",
+         c: "The Jungle Book"
+      },
+      correctAnswer: "c"
+   },
+   {
+      question: "In the 1971 movie, what gets stuck in the stones in the walkway when Wonka is first introduced?",
+      answers: {
+         a: "Wonka's cane",
+         b: "Veruca's snozzberry",
+         c: "Granpa Joe's robe"
+      },
+      correctAnswer: "a"
+   },
+   {
+      question: "Which musical artist covered the songs Pure Imagination, The Candy Man, I've got a Golden Ticket, and I Want It Now on his 2001 album, Songs I Heard?",
+      answers: {
+         a: "Eminem",
+         b: "Harry Connick, Jr",
+         c: "James Blunt"
+      },
+      correctAnswer: "b"
+   },
+   {
+      question: "What was the last name of Charlie, the main character?",
+      answers: {
+         a: "Bucket",
+         b: "Bouquet",
+         c: "Connick, Jr"
+      },
+      correctAnswer: "c"
+   }, 
+];
